1d268c48cebd77acdab1eee50dfd106a7895849d
kaiqui <kaiqui82@gmail.com>
2025-06-03 17:41:53 -0300

    add canary

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -8,16 +8,6 @@ output "ecs_cluster_name" {
   value       = aws_ecs_cluster.this.name
 }
 
-output "ecs_service_green_id" {
-  description = "ID (ARN) do ECS Service de produção"
-  value       = aws_ecs_service.app_green.id
-}
-
-output "ecs_service_blue_id" {
-  description = "ID (ARN) do ECS Service canary"
-  value       = aws_ecs_service.app_blue.id
-}
-
 output "task_definition_arn" {
   description = "ARN do Task Definition utilizado pelos Services"
   value       = aws_ecs_task_definition.app_with_datadog.arn
